a2be9dbdf1a5b0dac1942cdaf355bd30c2fbae0d
Sakda Aiamitsara <66160265@go.buu.ac.th>
2026-08-07 03:27:37 +0700

    feat: add MediaCard and MediaGrid components, connect Movies page to trending API

---
+++ frontend/src/components/MediaGrid.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import MediaCard from "./MediaCard"
+
+function MediaGrid({ mediaType }) {
+    const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const res = await fetch("http://localhost:4000/api/trending");
+                const data = await res.json();
+                const filtered = data.results.filter((item) => item.mediaType === mediaType);
+                setItems(filtered);
+            } catch (err) {
+                console.error("Failed to fetch trending:", err);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchData();
+    }, [mediaType]);
+
+    if (loading) return <p className="text-white">Loading...</p>;
+
+    return (
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-4">
+            {items.map((item)=> (
+                <MediaCard key={item.uniqueId} {...item} />
+            ))}
+        </div>
+    );
+}
+
+export default MediaGrid;
--- frontend/src/pages/Movies.jsx
+++ frontend/src/pages/Movies.jsx
@@ -1,10 +1,12 @@
 import SearchBar from "../components/SearchBar"
+import MediaGrid from "../components/MediaGrid"
 
 function Movies() {
     return (
         <>
         <h1>Movies Page</h1>
         <SearchBar />
+        <MediaGrid mediaType="movie" />
         </>
     )
 }
