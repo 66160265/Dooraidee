a5e653e8b2cf9ac0bd25ab0c447af96f67579d99
Sakda Aiamitsara <66160265@go.buu.ac.th>
2026-08-07 03:38:59 +0700

    feat: connect TvShows and Anime pages to trending API using MediaGrid

--- frontend/src/pages/Movies.jsx
+++ frontend/src/pages/Movies.jsx
@@ -1,14 +1,14 @@
-import SearchBar from "../components/SearchBar"
-import MediaGrid from "../components/MediaGrid"
+import SearchBar from "../components/SearchBar";
+import MediaGrid from "../components/MediaGrid";
 
 function Movies() {
-    return (
-        <>
-        <h1>Movies Page</h1>
-        <SearchBar />
-        <MediaGrid mediaType="movie" />
-        </>
-    )
+  return (
+    <>
+      <h1>Movies Page</h1>
+      <SearchBar />
+      <MediaGrid mediaType="movie" />
+    </>
+  );
 }
 
 export default Movies;
--- frontend/src/pages/TvShows.jsx
+++ frontend/src/pages/TvShows.jsx
@@ -1,12 +1,14 @@
-import SearchBar from "../components/SearchBar"
+import SearchBar from "../components/SearchBar";
+import MediaGrid from "../components/MediaGrid";
 
 function TvShows() {
-    return (
+  return (
     <>
-        <h1>TvShows Page</h1>
-        <SearchBar />
+      <h1>TvShows Page</h1>
+      <SearchBar />
+      <MediaGrid mediaType="tv" />
     </>
-    )
+  );
 }
 
 export default TvShows;
